be425117936812a7468e841a2449b5d5e2e9d87c
Arjunks2001 <arjun@firstfloor.agency>
2024-08-16 19:29:23 +0530

    Create card.js

---
+++ src/components/sample/card.js
@@ -0,0 +1,102 @@
+import React from "react";
+import EventImage from "../../../../Assets/Icons/EventImage.png";
+const SearchCard = () => {
+  return (
+    <div class=" ">
+    <div class="d-lg-flex justify-content-lg-evenly flex-wrap flex-column flex-lg-row">
+      <div class="col-lg-4  p-4">
+        <div class="card rounded-5 p-2">
+          <div class="card-image-wrapper">
+            <img src={EventImage} class="card-img-top img-fluid" alt="..."/>
+          </div>
+          <div class="card-body">
+          <div className='col-lg-9'>
+          <h5 class="card-text">Business Brilliance Competition</h5>
+          <small class="card-text">IIT Roorke</small>
+          </div>
+          
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4  p-4">
+        <div class="card rounded-5 p-2">
+          <div class="card-image-wrapper">
+            <img src={EventImage} class="card-img-top img-fluid" alt="..."/>
+          </div>
+          <div class="card-body">
+          <div className='col-lg-9'>
+          <h5 class="card-text">Business Brilliance Competition</h5>
+          <small class="card-text">IIT Roorke</small>
+          </div>
+          
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 p-4">
+        <div class="card rounded-5 p-2">
+          <div class="card-image-wrapper">
+            <img src={EventImage} class="card-img-top img-fluid" alt="..."/>
+          </div>
+          <div class="card-body">
+          <div className='col-lg-9'>
+          <h5 class="card-text">Business Brilliance Competition</h5>
+          <small class="card-text">IIT Roorke</small>
+          </div>
+          
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 p-4">
+        <div class="card rounded-5 p-2">
+          <div class="card-image-wrapper">
+            <img src={EventImage} class="card-img-top img-fluid" alt="..."/>
+          </div>
+          <div class="card-body">
+          <div className='col-lg-9'>
+          <h5 class="card-text">Business Brilliance Competition</h5>
+          <small class="card-text">IIT Roorke</small>
+          </div>
+          
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 p-4">
+        <div class="card rounded-5 p-2">
+          <div class="card-image-wrapper">
+            <img src={EventImage} class="card-img-top img-fluid" alt="..."/>
+          </div>
+          <div class="card-body">
+          <div className='col-lg-9'>
+          <h5 class="card-text">Business Brilliance Competition</h5>
+          <small class="card-text">IIT Roorke</small>
+          </div>
+          
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 p-4">
+        <div class="card rounded-5 p-2">
+          <div class="card-image-wrapper">
+            <img src={EventImage} class="card-img-top img-fluid" alt="..."/>
+          </div>
+          <div class="card-body">
+          <div className='col-lg-9'>
+          <h5 class="card-text">Business Brilliance Competition</h5>
+          <small class="card-text">IIT Roorke</small>
+          </div>
+          
+          </div>
+        </div>
+      </div>
+    
+     
+      
+    
+      
+     
+    </div>
+  </div>
+  );
+};
+
+export default SearchCard;
